fec68d7075b97da38f760b3b2e6a27b5306ca64c
Hani A <ha8012@yahoo.com>
2020-01-27 20:32:29 -0500

    adding image upload compenents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import FileUpload from './components/FileUpload';
 
 function App() {
   return (
     <div className='App'>
       <div className='navbar'> Image Vectorizer</div>
+      <div className='container'>
+        <FileUpload />
+      </div>
     </div>
   );
 }
