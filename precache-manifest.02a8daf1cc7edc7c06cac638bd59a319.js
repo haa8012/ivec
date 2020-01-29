cea7faf0f201db102303f7ffbd9fbd48e21a0a8c
Hani A <ha8012@yahoo.com>
2020-01-28 20:38:07 -0500

    Updates

---
+++ precache-manifest.02a8daf1cc7edc7c06cac638bd59a319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40edbe1a065fae995b7ed894466494d",
+    "url": "/ivec/index.html"
+  },
+  {
+    "revision": "8b0849db463064f5ec3e",
+    "url": "/ivec/static/css/main.51dd11c6.chunk.css"
+  },
+  {
+    "revision": "afdde84f6ff396c65981",
+    "url": "/ivec/static/js/2.2b1ecc8f.chunk.js"
+  },
+  {
+    "revision": "fbeaffac2e79d8c776ae7d710da0e23b",
+    "url": "/ivec/static/js/2.2b1ecc8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b0849db463064f5ec3e",
+    "url": "/ivec/static/js/main.09256dc9.chunk.js"
+  },
+  {
+    "revision": "87f9968ebdc23bdfc853",
+    "url": "/ivec/static/js/runtime-main.377e6c22.js"
+  },
+  {
+    "revision": "952ab162764bdb818f9a03abaed32a80",
+    "url": "/ivec/static/media/exchange.952ab162.svg"
+  }
+]);
